7ca60317adcec3802ec7d93dc50dd3ea6d78f6cd
AnuPrasad78 <anup@emids.com>
2026-09-09 17:24:39 +0530

    Delete event_managment directory

--- event_managment/server/config/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    logging: false, // Disable logging; default: console.log
-}
